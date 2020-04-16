4a40deb6658ecd018d86d68554ceaca0999622ab
Jordan Suero <9836321+jtsuero@users.noreply.github.com>
2020-04-16 12:30:42 -0700

    test format

--- app/src/AreaMap.js
+++ app/src/AreaMap.js
@@ -1,19 +1,18 @@
-import React, { Component } from 'react';
-import GoogleMapReact from 'google-map-react';
-import Api from './api.js';
-import Icons from './Icons.js';
-import marker from './location_map_pin_navy_blue5.png'
-
+import React, { Component } from "react";
+import GoogleMapReact from "google-map-react";
+import Api from "./api.js";
+import Icons from "./Icons.js";
+import marker from "./location_map_pin_navy_blue5.png";
 
 class AreaMap extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       longitude: null,
       latitude: null,
       zoom: 12,
-      businessList: null,
-    }
+      businessList: null
+    };
   }
 
   componentDidMount() {
@@ -22,53 +21,59 @@ class AreaMap extends Component {
 
   getLocation = () => {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
+      navigator.geolocation.getCurrentPosition(position => {
         const longitude = position.coords.longitude;
         const latitude = position.coords.latitude;
-        this.setState({longitude, latitude})
+        this.setState({ longitude, latitude });
       });
     } else {
-      console.log('error with navigator');
+      console.log("error with navigator");
     }
-  }
+  };
 
   //passes businessInfo back to parent component -- App.js
-  onPhotoClick = (businessInfo) => {
+  onPhotoClick = businessInfo => {
     this.props.clickPhoto(businessInfo);
-  }
+  };
 
   onMapClick = () => {
     this.props.closeBottomBar();
-  }
+  };
 
   render() {
-    if(this.state.latitude === null || this.state.longitude === null) {
-      return(
-        <div>
-          loading
-        </div>
-      )
+    if (this.state.latitude === null || this.state.longitude === null) {
+      return <div>loading</div>;
     }
     const mapProps = {
       center: {
         lat: this.state.latitude,
-        lng: this.state.longitude,
+        lng: this.state.longitude
       },
       zoom: this.state.zoom,
-      key: 'AIzaSyC3qAdwyGSoamVwR7DIS5VdmhVZlg1NBic',
-    }
+      key: "AIzaSyC3qAdwyGSoamVwR7DIS5VdmhVZlg1NBic"
+    };
 
-    if(this.state.businessList === null) {
-      Api.getBusinesses(this.state.latitude, this.state.longitude)
-        .then(data => this.setState({businessList: data.businesses}))
+    if (this.state.businessList === null) {
+      Api.getBusinesses(this.state.latitude, this.state.longitude).then(data =>
+        this.setState({ businessList: data.businesses })
+      );
     }
-    const PositionMarker = ({ text }) => <img className='marker' src={marker} alt={"You"}></img>;
-    if(this.state.businessList === null) return null
+    const PositionMarker = ({ text }) => (
+      <img className="marker" src={marker} alt={"You"}></img>
+    );
+    if (this.state.businessList === null) return null;
     return (
       // Important! Always set the container height explicitly
-      <div className='map-container' onClick={this.onMapClick}>
-        <GoogleMapReact bootstrapURLKeys={{ key: mapProps.key }} defaultCenter={mapProps.center} defaultZoom={mapProps.zoom}>
-          <PositionMarker lat={this.state.latitude} lng={this.state.longitude}  />
+      <div className="map-container" onClick={this.onMapClick}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: mapProps.key }}
+          defaultCenter={mapProps.center}
+          defaultZoom={mapProps.zoom}
+        >
+          <PositionMarker
+            lat={this.state.latitude}
+            lng={this.state.longitude}
+          />
           {this.state.businessList.map((restaurant, index) => {
             return (
               <Icons
@@ -77,14 +82,16 @@ class AreaMap extends Component {
                 lng={restaurant.coordinates.lng}
                 data={restaurant}
                 clickPhoto={this.onPhotoClick.bind(this)}
-
               />
-
-            )
+            );
           })}
         </GoogleMapReact>
       </div>
     );
+  }
+}
+
+export default AreaMap;
   }
 }
 
