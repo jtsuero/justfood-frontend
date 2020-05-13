acb167e39c9a0d308c603b0fd14aa40dd4dc988c
Jordan Suero <9836321+jtsuero@users.noreply.github.com>
2020-05-13 14:59:50 -0700

    fix local build

--- app/src/api.js
+++ app/src/api.js
@@ -1,14 +1,17 @@
-const HOST_URL = "http://localhost:8000";
+const HOST_URL =
+  window.location.hostname === 'localhost'
+    ? 'http://localhost:8000'
+    : 'http://justfoodserver-env.eba-ajm3vhxu.us-west-2.elasticbeanstalk.com';
 
 class Api {
   getBusinesses = searchParams => {
     return fetch(
       `${HOST_URL}/restaurants/?long=${searchParams.longitude}&lat=${searchParams.latitude}&keyword=${searchParams.keyword}&radius=${searchParams.radius}`,
       {
-        method: "get",
-        mode: "cors",
-        headers: { "Content-Type": "application/json" }
-      }
+        method: 'get',
+        mode: 'cors',
+        headers: {'Content-Type': 'application/json'},
+      },
     ).then(res => res.json());
   };
 
@@ -16,10 +19,10 @@ class Api {
     return fetch(
       `${HOST_URL}/places/yelp/?name=${attributes.name}&address1=${attributes.address1}&city=${attributes.city}&state=${attributes.state}&zip_code=${attributes.zip}&phone=${attributes.phone}&lat=${attributes.lat}&lng=${attributes.lng}`,
       {
-        method: "get",
-        mode: "cors",
-        headers: { "Content-Type": "application/json" }
-      }
+        method: 'get',
+        mode: 'cors',
+        headers: {'Content-Type': 'application/json'},
+      },
     ).then(res => res.json());
   };
 }
